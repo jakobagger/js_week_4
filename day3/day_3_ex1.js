dad01ce3e21cc8f66e482d8c482ab6a0a03c9cea
jakobagger <113458342+jakobagger@users.noreply.github.com>
2023-09-23 02:14:25 +0200

    day3 ex1 functional
    TO_DO reflections

--- day3/day_3_ex1.js
+++ day3/day_3_ex1.js
@@ -2,11 +2,33 @@ const SERVER_URL = "https://jsonplaceholder.typicode.com/users"
 
 document.getElementById("find-user-btn").addEventListener("click", getUserByID)
 
+document.getElementById("get-all-users-btn").addEventListener("click", getAllUsers)
+
 function getUserByID(){
     const id = document.getElementById("user-id-input").value
     fetch(SERVER_URL+"/"+id)
-    .then(res => {if(!res.ok){return alert("Not found")} else {
+    .then(res => {if(!res.ok){return alert("User not found")} else {
         return res.json()}
       })
-      .then(user => {document.getElementById("single-user").innerText = JSON.stringify(user, null, 3)})
+      .then(user => {
+        document.getElementById("single-user").innerText = JSON.stringify(user, null, 3)
+        document.getElementById("single-user").style.display = "block"})
+      .catch(e => console.log(e))
+}
+
+function getAllUsers(){
+  fetch(SERVER_URL)
+  .then(res => {if(!res.ok){return alert("Could not get users")} else {
+    return res.json()}
+  })
+  .then(users => {
+    const userTable = users.map(user => 
+    `<tr>
+      <td>${user.name}</td>
+      <td>${user.phone}</td>
+    </tr>`).join('')
+    console.log(userTable)
+    document.getElementById("user-table-body").innerHTML = userTable
+    document.getElementById("users-table").style.display = "block"
+  }).catch(e => console.log(e))
 }
